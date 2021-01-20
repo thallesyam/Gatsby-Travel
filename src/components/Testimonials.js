2ed02e88a71caae1c8ceaafbaa863605d895cc55
Thalles Ian <thallesyam@users.=.github.com>
2021-01-20 20:01:00 -0300

    Testimonials section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,141 @@
 import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from 'gatsby'
+import { IoMdCheckmarkCircleOutline } from 'react-icons/io'
+import { FaRegLightbulb } from 'react-icons/fa'
 
 const Testimonials = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: {ext: {regex: "/(jpeg)|(jpg)|(png)/"}, name: {in: ["testimonials-1", "testimonials-2"]}}) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+
+  `)
+
   return (
-    <div>
-      <h1>Testimonials</h1>
-    </div>
+    <TestimonialsContainer>
+      <TopLine>
+        Testemunhas
+      </TopLine>
+      <Description>
+        O que as pessoas estão dizendo
+      </Description>
+      <ContentWrapper>
+        <ColumnOne>
+          <Testimonial>
+            <IoMdCheckmarkCircleOutline  css={`color: #3fffa8; font-size: 2rem; margin-bottom: 1rem;`}/>
+            <h3>Maicon</h3>
+            <p>
+              {" "}
+              "A melhor experiencia da minha vida, foi muito legal conhecer
+              as montanhas, e não tive dor de cabeça nenhuma na viagem e nem na 
+              hospedagem."
+            </p>
+          </Testimonial>
+          <Testimonial>
+            <FaRegLightbulb css={`color: #f9b19b; font-size: 2rem; margin-bottom: 1rem;`}/>
+            <h3>Sarah</h3>
+            <p>
+              "A melhor experiencia da minha vida, foi muito legal conhecer
+              as montanhas, e não tive dor de cabeça nenhuma na viagem e nem na 
+              hospedagem."
+            </p>
+          </Testimonial>
+        </ColumnOne>
+        <ColumnTwo>
+          {data.allFile.edges.map((img, key) => (
+            <Images key={key} fluid={img.node.childImageSharp.fluid} alt={img.node.alt} />
+
+          ))}
+        </ColumnTwo>
+      </ContentWrapper>
+    </TestimonialsContainer>
   )
 }
 
 export default Testimonials
+
+
+const TestimonialsContainer = styled.div`
+  width: 100%;
+  background: #fcfcfc;
+  color: #000;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  height: 100%;
+`
+
+const TopLine = styled.p`
+  color: #077bf1;
+  font-size: 1rem;
+  padding-left: 2rem;
+  margin-bottom: 0.75rem;
+
+`
+
+const Description = styled.p`
+  text-align: start;
+  padding-left: 2rem;
+  margin-bottom: 4rem;
+  font-size: clamp(1.5rem, 5vw, 2rem);
+  font-weight: bold;
+
+`
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ColumnOne = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+`
+
+const Testimonial = styled.div`
+  padding-top: 1rem;
+  padding-right: 2rem;
+
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-style: italic;
+  }
+
+  p {
+    color: #3b3b3b;
+  }
+
+`
+
+const ColumnTwo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 2rem;
+  grid-gap: 10px;
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const Images = styled(Img)`
+  border-radius: 10px;
+  height: 100%;
+
+`
